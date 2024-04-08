06dbc49e5776f02ce75008b80f5312365394fa2a
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2024-04-08 17:35:21 -0600

    cambios

--- app/Http/Middleware/DomainAccess.php
+++ app/Http/Middleware/DomainAccess.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use App\Models\User;
 class DomainAccess
 {
     /**
@@ -16,33 +16,38 @@ class DomainAccess
     public function handle(Request $request, Closure $next): Response
     {
 
-        If($request->user()->hasRole(1))
+        $userId = session('user_id');
+
+        
+        $user = User::find($userId);
+
+
+        if($user->rol_id === 3)
         {
+            return $next($request);
+        }
+        else if($user->rol_id === 2)
+        {
+            if($request->getHost() == 'danielypablo.tech' || $request->getHost() == '192.168.25.2')
+            {
+                return $next($request);
+            }
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+            return redirect()->route('login.view')->with('status','Su cuenta no ha sido verificada');
+        }
+        else if($$user->rol_id == 1)
+        {
+
             if($request->getHost() == '192.168.25.2')
             {
-                return $next($request);
+
             }
             $request->session()->invalidate();
             $request->session()->regenerateToken();
             return redirect()->route('login.view')->with('status','Su cuenta no ha sido verificada');
         }
-        else if($request->user()->hasRole(2))
-        {
-            if($request->getHost() == 'danielypablo.tech')
-            {
-                return $next($request);
-            }
-            $request->session()->invalidate();
-            $request->session()->regenerateToken();
-            return redirect()->route('login.view')->with('status','Su cuenta no ha sido verificada');
-        }
-        else if($request->user()->hasRole(3))
-        {
-            if($request->getHost() == 'danielypablo.tech')
-            {
-                return $next($request);
-            }
-        }
+
 
         // if ($request->user()->hasRole($role)) 
         // {
