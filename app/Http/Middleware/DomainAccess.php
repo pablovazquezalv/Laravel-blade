1f6d2c738dd755b97d76a874c6640e047f9c5454
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2024-04-08 17:29:20 -0600

    cambios

--- app/Http/Middleware/DomainAccess.php
+++ app/Http/Middleware/DomainAccess.php
@@ -16,7 +16,7 @@ class DomainAccess
     public function handle(Request $request, Closure $next): Response
     {
 
-        if($request->user()->rol_id == 3)
+        if($request->user()->rol_id === 3)
         {
             return $next($request);
         }
