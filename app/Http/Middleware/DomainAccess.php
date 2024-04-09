1079d1b01b933a288a58ebf883a6184852a08745
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2024-04-08 19:15:47 -0600

    cambios

--- app/Http/Middleware/DomainAccess.php
+++ app/Http/Middleware/DomainAccess.php
@@ -44,7 +44,7 @@ class DomainAccess
                 
                   if($request->getHost() == 'danielypablo.tech' || $request->getHost() == '192.168.25.2')
                   {
-                    dd($rol);
+                    
                       return $next($request);
                   }
                   $request->session()->invalidate();
