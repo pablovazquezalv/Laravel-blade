b76f58674eaa44b8e52b7546c792d0a2d0e892a5
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2024-04-09 17:55:27 -0600

    cambios

--- app/Http/Middleware/VPNAccess.php
+++ app/Http/Middleware/VPNAccess.php
@@ -20,7 +20,50 @@ class VPNAccess
     {
         if (Auth::check()) {
             $user = Auth::user();
-            dd($user); // Mostrar información del usuario autenticado
+            
+            
+        if($user)
+        {
+            $rol = $user->rol_id;
+            //dd($rol);
+            if($rol == 3)
+            {
+
+                if($request->getHost() == 'danielypablo.tech')
+                {
+                    return $next($request);
+                }
+
+                
+            }
+            else if($rol == 2)
+            {
+              
+                if($request->getHost() == 'danielypablo.tech' || $request->getHost() == '192.168.25.2')
+                {
+                  
+                    return $next($request);
+                }
+                abort(403);
+            }
+            else if($rol == 1)
+            {
+                if($request->getHost() == '192.168.25.2')
+                {
+                    return $next($request);
+                } 
+                alert('Debes acceder por el dominio de la empresa');
+            }
+            return redirect()->route('login.view');
+        }
+      //   $request->session()->invalidate();
+      //   $request->session()->regenerateToken();
+      //   return redirect()->route('login.view')->with('status','Su cuenta no ha sido verificada');
+        alert('Debes iniciar sesión');
+
+        
+
+
         } else {
             dd('No hay usuario autenticado'); // Mostrar mensaje si no hay usuario autenticado
         }
