faa661650363f745bd755aebfbe6fc84d2e5c177
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2024-04-09 22:55:50 -0600

    cambios

--- app/Http/Middleware/VPNAccess.php
+++ app/Http/Middleware/VPNAccess.php
@@ -33,7 +33,7 @@ class VPNAccess
             {
                 return $next($request);
             }
-            else if($rol == 3 && $request->ip() != '192.168.25.13')
+            else if($rol == 3 && request()->getHost() == '192.168.25.2')
             {
                 return $next($request);
             }
